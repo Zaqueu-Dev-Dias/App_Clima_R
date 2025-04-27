8d5dfcb3c361f3f94887d14458e5b68acafcc385
Zaqueu-Dev-Dias <zaqueudev0@gmail.com>
2025-04-27 15:05:41 -0300

    Estrutura básica do app.
    Nesse commit, vai ter a estrutura básica do app.

---
+++ Pasta_do_app/lib/Tela_principal.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class Tela_Principal extends StatelessWidget {
+  const Tela_Principal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          color: Colors.blue,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Container(
+                width: 300,
+                child: Material(
+                  color: Colors.transparent,
+                    child: TextField(
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(width: 3),
+                          borderRadius: BorderRadius.circular(15))),
+                )),
+              ),
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 60, right: 10, top: 20),
+                  child: Text(
+                    '26/04/2025',
+                    style: TextStyle(
+                        fontSize: 20, decoration: TextDecoration.none),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 60, left: 10, top: 20),
+                  child: Text(
+                    '11:53:34',
+                    style: TextStyle(
+                        fontSize: 20, decoration: TextDecoration.none),
+                  ),
+                ),
+              ],
+            ),
+            Text(
+              'Emoji',
+              style: TextStyle(fontSize: 60, decoration: TextDecoration.none),
+            ),
+            Text(
+              '25°C',
+              style: TextStyle(fontSize: 100, decoration: TextDecoration.none),
+            ),
+            Center(
+                child: Text(
+              'Guarulhos',
+              style: TextStyle(fontSize: 20, decoration: TextDecoration.none),
+            )),
+            Padding(
+              padding: const EdgeInsets.only(top: 25),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Humidade:',
+                      style: TextStyle(
+                          fontSize: 20, decoration: TextDecoration.none),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Vento:',
+                      style: TextStyle(
+                          fontSize: 20, decoration: TextDecoration.none),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Céu:',
+                      style: TextStyle(
+                          fontSize: 20, decoration: TextDecoration.none),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Continente:',
+                      style: TextStyle(
+                          fontSize: 20, decoration: TextDecoration.none),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
